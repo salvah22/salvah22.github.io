a9af3ae3d936f524e806c059461bcb413b4f5890
salvah22 <salvah22@gmail.com>
2023-01-18 13:10:55 +0000

    small updates

--- s.js
+++ s.js
@@ -1,39 +1,39 @@
+let nav
 function hideAllContents(){
     var elements = document.getElementsByClassName('content');
-    for(var i = 0, length = elements.length; i < length; i++) {
-        elements[i].style.display = 'none';
+    for(e of elements) {
+        e.style.display = 'none';
     }
 }
 function bodyLoad(){
-    hideAllContents();
-    document.getElementById('nav').style.display = 'none'; // hide nav on document ready
-    document.getElementById('blinka').classList.add('blink'); // make GIS blink
-    setTimeout('document.getElementById("blinka").classList.remove("blink");', 10000); // and stop GIS blinking after X miliseconds
+    nav = document.getElementById('nav')
 }
 function navClick(){
-    // scroll up when nav is clicked and hide all contents
-    document.getElementById('nav').style.display = 'none';
+    // hide all contents
+    nav.style.display = 'none';
     hideAllContents();
+    // scroll up 
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 function contentCheck(id){
     // A single method to show element "content_" + button pressed id
-    if (document.getElementById('content_' + id).style.display === 'none'){
-        document.getElementById('content_' + id).style.display = 'block';
-        document.getElementById('nav').style.display = 'block';
+    const content = document.getElementById('content_' + id)
+    if (content.style.display === 'none'){
+        content.style.display = 'block';
+        nav.style.display = 'block';
     } else {
-        document.getElementById('content_' + id).style.display = 'none';
+        content.style.display = 'none';
         var visibleBoxes = 0;
         // if all content class divs are hidden, hide nav
         var elements = document.getElementsByClassName('content');
-        for(var i = 0, length = elements.length; i < length; i++) {
-            if (elements[i].style.display !== 'none'){
+        for(e of elements) {
+            if (e.style.display !== 'none'){
                 visibleBoxes += 1;
             }
         }
         if (visibleBoxes == 0){
-            document.getElementById('nav').style.display = 'none';
+            nav.style.display = 'none';
         }
     }
 }
